1b1eb9128a2640d9a81b4590f9039c5aa2e2e0ab
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-14 19:16:38 +0530

    Create GetPairNumber.h

---
+++ GetPairNumber.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <main.h>
+
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber);
