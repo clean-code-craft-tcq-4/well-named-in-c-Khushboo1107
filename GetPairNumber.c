341045d4ef2e696b140c02080e5e25b8752b0583
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-14 19:10:15 +0530

    Update GetPairNumber.c

--- GetPairNumber.c
+++ GetPairNumber.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
+#include <main.h>
+extern ColorPair colorPair_cp;
 
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
