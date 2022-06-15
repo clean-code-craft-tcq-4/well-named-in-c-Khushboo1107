1cc52784d41489405febe0636df1ca3fe08ed04f
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-15 18:53:41 +0530

    Update GetPairNumber.c

--- GetPairNumber.c
+++ GetPairNumber.c
@@ -2,6 +2,10 @@
 #include <assert.h>
 #include "main.h"
 
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
