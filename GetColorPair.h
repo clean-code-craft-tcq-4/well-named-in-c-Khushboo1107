c226e70756286193705c05498e12e17449077658
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-15 15:23:03 +0530

    Delete GetColorPair.h

--- GetColorPair.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <main.h>
-
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
-ColorPair GetColorFromPairNumber(int pairNumber);
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor);
