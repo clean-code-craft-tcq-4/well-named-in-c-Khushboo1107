35a401152d6fd56526da6758f8359fa7c7623946
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-15 23:50:25 +0530

    Update manual.c

--- manual.c
+++ manual.c
@@ -5,13 +5,13 @@
 extern const int MAX_COLORPAIR_NAME_CHARS;
 
 void GetColorManual(void)
-{ printf("%d",1);
-        //for(int i=1; i<=25;i++)
-      //  {
-    //    ColorPair info = GetColorFromPairNumber(i);
- // char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-   // ColorPairToString(&info, colorPairNames);
-      //    printf("%d", i);
-                
-       // }
+{  printf("Reference Manual for Color Coding\n\n");
+    int PairNumber;
+    for(PairNumber=1; PairNumber <= 25; PairNumber++)
+    {
+        ColorPair colors = GetColorFromPairNumber(PairNumber);
+        char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+        ColorPairToString(&colors, colorPairNames);
+        printf("Color Pair:%d\t\t Major Minor Color Pair:%s\n",PairNumber,colorPairNames );
+    }
 }
