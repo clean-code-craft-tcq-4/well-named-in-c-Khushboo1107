7fc36aa7d8dda724d764603791383b874c37d890
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-15 20:03:47 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,36 @@
 #include "main.h"
 
 int main() {
-    const int MAX_COLORPAIR_NAME_CHARS = 16;
+    const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
 
-      for(int i=0; i<MAX_COLORPAIR_NAME_CHARS;i++)
+      for(int i=1; i<=25;i++)
            {
-                GetColorManual(&colorPair, colorPairNames);
+               for( int j=0; j<numberOfMajorColors; j++)
+               {
+                   for( int k=0; j<numberOfMinorColors; k++)
+                   {
+                       printf("%d",i);
+                       testNumberToPair(i,j,k);
+                   }
+               }
             }
     return 0;
 }
