edd282364820fd0637a827412ed31b2cad30611d
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-15 15:49:01 +0530

    Update GetPairNumber.c

--- GetPairNumber.c
+++ GetPairNumber.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
 #include "main.h"
-extern ColorPair colorPair;
 
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
@@ -13,6 +12,7 @@ void testPairToNumber(
     enum MinorColor minor,
     int expectedPairNumber)
 {
+    ColorPair colorPair;
     colorPair.majorColor = major;
     colorPair.minorColor = minor;
     int pairNumber = GetPairNumberFromColor(&colorPair);
