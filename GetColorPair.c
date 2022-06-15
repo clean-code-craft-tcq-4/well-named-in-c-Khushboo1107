b7be7c3812814d39f8df4335f0839e2c6f33ba5d
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-15 18:12:16 +0530

    Update GetColorPair.c

--- GetColorPair.c
+++ GetColorPair.c
@@ -3,8 +3,8 @@
 #include "main.h"
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
-        "abc"],
-        "abc"]);
+        'abc'],
+        'abc']);
 }
 
 ColorPair GetColorFromPairNumber(int pairNumber) {
