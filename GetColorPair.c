819cd20ac0566c76b905fdcbc7af63d6ae551c7a
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-15 14:31:28 +0530

    Update GetColorPair.c

--- GetColorPair.c
+++ GetColorPair.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
-#include <main.h>
-#include <GetColorPair.h>
+#include "main.h"
+#include "GetColorPair.h"
 extern ColorPair colorPair_cp;
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
