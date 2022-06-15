f32cdb0f0eefd04c8d090fb283c7b0ee9b71bf16
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-15 15:22:47 +0530

    Update main.h

--- main.h
+++ main.h
@@ -18,3 +18,15 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber);
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor);
