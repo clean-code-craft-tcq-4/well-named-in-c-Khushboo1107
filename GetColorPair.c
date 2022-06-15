cb85abd10422a6b51b8040978756df6f5d88080c
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-15 18:51:00 +0530

    Update GetColorPair.c

--- GetColorPair.c
+++ GetColorPair.c
@@ -12,9 +12,6 @@ const char* MinorColorNames[] = {
 int numberOfMajorColors =
     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
