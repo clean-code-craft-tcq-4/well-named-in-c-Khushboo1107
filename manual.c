48f8caa51360003cfdc867ff7f31c1d1648195d4
Khushboo1107 <105203869+Khushboo1107@users.noreply.github.com>
2022-06-17 09:43:54 +0530

    Update manual.c

--- manual.c
+++ manual.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
-#include <assert.h>
 #include "main.h"
 #include "manual.h"
 extern const int MAX_COLORPAIR_NAME_CHARS;
 
-void GetColorManual(void)
+void GetColorManual()
 {  printf("Reference Manual for Color Coding\n\n");
     int PairNumber;
     for(PairNumber=1; PairNumber <= 25; PairNumber++)
